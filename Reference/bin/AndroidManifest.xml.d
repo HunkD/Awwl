7db60a01f4dcbddc4dcda30a87f432dada0b6d03
HunkDeng <niknow@163.com>
2014-11-02 19:05:46 +0800

    TK-1-CLEAN: clean bin gen .project

--- Reference/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-J:\Useful documents\java Projects\Github\Android-WhiteLabel-Lab\Reference\bin\AndroidManifest.xml : \
-J:\Useful documents\java Projects\Github\Android-WhiteLabel-Lab\Reference\AndroidManifest.xml \
--- Reference/gen/com/hunk/reference/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.hunk.reference;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = false;
-}
