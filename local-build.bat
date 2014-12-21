cea682e9ef4b239e216004de4b4778abbe8334ff
Yuxuan Deng <niknow@live.com>
2014-12-21 14:41:55 +0800

    Revert "TK-13: Configure jenkins to pull the code from Github and build apk from..."

--- local-build.bat
+++ local-build.bat
@@ -1,8 +1,6 @@
-#cd "J:\Useful documents\java Projects\Github\Android-WhiteLabel-Lab"
+cd "J:\Useful documents\java Projects\Github\Android-WhiteLabel-Lab"
 call android update project --name "Appcompat-v7" --path \appcompat
 call android update project --name "Nobank" --path \Nobank --library "..\appcompat"
-call android update project --name "StubServer" --path \StubServer --library "..\Nobank"
 call android update project --name "ReferenceBank" --path \Reference --library "..\Nobank"
-call android update project --name "ReferenceBank" --path \Reference --library "..\StubServer"
-#cd "Reference"
-#call ant clean release > release.log
+cd "Reference"
+call ant clean release > release.log
