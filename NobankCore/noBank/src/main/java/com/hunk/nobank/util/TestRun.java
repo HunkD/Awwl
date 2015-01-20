8225e962ed955add455a209f717edf8d032ab2b0
Hunk <niknow@live.com>
2015-01-20 22:36:32 +0800

    Gradle: Test run java application.
    Execute main method in Android project with Java application runner.

---
+++ NobankCore/noBank/src/main/java/com/hunk/nobank/util/TestRun.java
@@ -0,0 +1,11 @@
+package com.hunk.nobank.util;
+
+/**
+ * Created by HunkDeng on 2015/1/18.
+ */
+public class TestRun {
+    public static void main(String[] args) {
+        StringUtils.isNullOrEmpty("123");
+        System.out.println("finish");
+    }
+}
