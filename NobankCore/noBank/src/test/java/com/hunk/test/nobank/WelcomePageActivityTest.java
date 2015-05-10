540858d5b865c012641473b3158705ab7bd6f089
Hunk Deng <niknow@live.com>
2015-05-10 12:15:15 +0800

    Add unit test case to WelcomeActivity

--- NobankCore/noBank/src/test/java/com/hunk/test/nobank/WelcomePageActivityTest.java
+++ NobankCore/noBank/src/test/java/com/hunk/test/nobank/WelcomePageActivityTest.java
@@ -1,56 +1,92 @@
 package com.hunk.test.nobank;
+
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.ResolveInfo;
 import android.os.Build;
-import android.provider.Settings;
 
 import com.hunk.nobank.BaseActivity;
+import com.hunk.nobank.BuildConfig;
 import com.hunk.nobank.NConstants;
 import com.hunk.nobank.R;
 import com.hunk.nobank.feature.Feature;
-import com.hunk.nobank.feature.login.activity.LoginPageActivity;
 import com.hunk.nobank.feature.welcome.activity.WelcomePageActivity;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
-import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
+import org.robolectric.manifest.ActivityData;
+import org.robolectric.manifest.AndroidManifest;
+import org.robolectric.manifest.IntentFilterData;
 import org.robolectric.res.builder.RobolectricPackageManager;
 import org.robolectric.shadows.ShadowActivity;
+import org.robolectric.shadows.ShadowApplication;
+
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(RobolectricTestRunner.class)
-@Config(manifest = "noBank/src/main/AndroidManifest.xml", emulateSdk = Build.VERSION_CODES.JELLY_BEAN)
+/**
+ * Please set the working directory for this unit test configuration is 'NobankCore/nobank'
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+/**Only support JELLY_BEAN isn't good :( **/
+@Config(constants = BuildConfig.class, emulateSdk = Build.VERSION_CODES.JELLY_BEAN)
 public class WelcomePageActivityTest {
+
+    /**
+     * This is for add all implicit intent to Package Manager,
+     * so Intent.resolveActivity() can run normally in our code.
+     */
+    @Before
+    public void setup() {
+        RobolectricPackageManager rpm =
+                (RobolectricPackageManager) RuntimeEnvironment.application.getPackageManager();
+
+        rpm.addManifest(ShadowApplication.getInstance().getAppManifest(), ShadowApplication.getInstance().getResourceLoader());
+
+        AndroidManifest appManifest = ShadowApplication.getInstance().getAppManifest();
+        for (Map.Entry<String, ActivityData> activity : appManifest.getActivityDatas().entrySet()) {
+            String activityName = activity.getKey();
+            ActivityData activityData = activity.getValue();
+            if (activityData.getTargetActivity() != null) {
+                activityName = activityData.getTargetActivityName();
+            }
+
+            ApplicationInfo applicationInfo = new ApplicationInfo();
+            applicationInfo.packageName = RuntimeEnvironment.application.getPackageName();
+            ResolveInfo resolveInfo = new ResolveInfo();
+            resolveInfo.resolvePackageName = RuntimeEnvironment.application.getPackageName();
+            resolveInfo.activityInfo = new ActivityInfo();
+            resolveInfo.activityInfo.applicationInfo = applicationInfo;
+            resolveInfo.activityInfo.name = activityName;
+
+            for (IntentFilterData intentFilterData : activityData.getIntentFilters()) {
+                for (String intentFilterAction : intentFilterData.getActions()) {
+                    Intent i = new Intent(intentFilterAction);
+                    i.setPackage(RuntimeEnvironment.application.getPackageName());
+                    rpm.addResolveInfoForIntent(i, resolveInfo);
+                }
+            }
+        }
+    }
+
     @Test
     public void testClickOnSignInButton() {
-        RobolectricPackageManager rpm =
-                (RobolectricPackageManager) Robolectric.application.getPackageManager();
         Intent gotoLogin = new Intent();
-        gotoLogin.setPackage(Robolectric.getShadowApplication().getPackageName());
+        gotoLogin.setPackage(RuntimeEnvironment.application.getPackageName());
         gotoLogin.setAction(BaseActivity.generateAction(Feature.login, NConstants.OPEN_MAIN));
-
-        ResolveInfo info = new ResolveInfo();
-        info.isDefault = true;
-
-        ApplicationInfo applicationInfo = new ApplicationInfo();
-        applicationInfo.packageName = Robolectric.getShadowApplication().getPackageName();
-        info.activityInfo = new ActivityInfo();
-        info.activityInfo.applicationInfo = applicationInfo;
-        info.activityInfo.name = LoginPageActivity.class.getName();
-
-        rpm.addResolveInfoForIntent(gotoLogin, info);
 
         WelcomePageActivity activity = Robolectric.setupActivity(WelcomePageActivity.class);
         activity.findViewById(R.id.welcome_btn_sign_in).performClick();
 
-        Robolectric.runUiThreadTasksIncludingDelayedTasks();
-
-        ShadowActivity si = Robolectric.shadowOf(activity);
+        ShadowActivity si = Shadows.shadowOf(activity);
         assertThat(si.getNextStartedActivity()).isEqualTo(gotoLogin);
     }
 }
