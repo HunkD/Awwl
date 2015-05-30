3b55d4dbd0086070d23c4d7e62d40241a7e147ab
Hunk Deng <niknow@live.com>
2015-05-30 09:43:24 +0800

    TK-36: Enhance network layer 1. When activity has been recycled, we need check if activity still alive when callback return. Avoid NullPointerException occurs.

---
+++ NobankCore/noBank/src/main/java/com/hunk/nobank/manager/ViewManagerListener.java
@@ -0,0 +1,35 @@
+package com.hunk.nobank.manager;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Since Android will recycle the activity or fragment sometimes, but our listener is holding in manager layer.
+ * Which means when activity has been recycled and Callback return at same time,
+ * if call any method like 'dismissDialog' in callback method, it will provoke NullPointerException.
+ */
+public abstract class ViewManagerListener implements ManagerListener {
+
+    private final WeakReference<Object> weakRefer;
+
+    public ViewManagerListener(Object o) {
+        this.weakRefer = new WeakReference<>(o);
+    }
+
+    @Override
+    public void success(String managerId, String messageId, Object data) {
+        if (weakRefer.get() != null) {
+            onSuccess(managerId, messageId, data);
+        }
+    }
+
+    @Override
+    public void failed(String managerId, String messageId, Object data) {
+        if (weakRefer.get() != null) {
+            onFailed(managerId, messageId, data);
+        }
+    }
+
+    public abstract void onSuccess(String managerId, String messageId, Object data);
+
+    public abstract void onFailed(String managerId, String messageId, Object data);
+}
