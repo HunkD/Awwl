640e73da90d399af425e585346ca4b6f4f8fb4f3
Hunk Deng <niknow@live.com>
2015-09-12 16:34:51 +0800

    [Astub] Add log to show that astub stops working.

--- NobankCore/astub/src/main/java/com/hunk/astub/MainActivity.java
+++ NobankCore/astub/src/main/java/com/hunk/astub/MainActivity.java
@@ -2,6 +2,7 @@ package com.hunk.astub;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,6 +14,7 @@ import java.io.IOException;
 public class MainActivity extends Activity {
 
     private static final int SERVICE_PORT = 8466;
+    private static final String TAG = "ASTUB";
     private Button mServiceBtn;
     private MyHTTPD mMyHTTPD;
 
@@ -43,5 +45,12 @@ public class MainActivity extends Activity {
                 }
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d(TAG, "Main Activity has been destroyed.");
     }
 }
