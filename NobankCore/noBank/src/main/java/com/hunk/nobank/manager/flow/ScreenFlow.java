0429466884398dc1c64e120bee6d73eb9f37873a
Hunk Deng <niknow@live.com>
2016-02-09 08:36:30 +0800

    [nobank] add condition in next() and start() method.

--- NobankCore/noBank/src/main/java/com/hunk/nobank/manager/flow/ScreenFlow.java
+++ NobankCore/noBank/src/main/java/com/hunk/nobank/manager/flow/ScreenFlow.java
@@ -1,6 +1,10 @@
 package com.hunk.nobank.manager.flow;
 
 import android.app.Activity;
+import android.content.Intent;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author HunkDeng
@@ -9,20 +13,32 @@ import android.app.Activity;
 public abstract class ScreenFlow {
 
     private final ScreenFlowManager mScreenFlowManager;
-    private Class<? extends Activity> mStartedActivity;
+    private Map<Class<? extends Activity>, ScreenNode> mScreenNodeMap = new HashMap<>();
 
     public ScreenFlow(ScreenFlowManager screenFlowManager) {
         mScreenFlowManager = screenFlowManager;
     }
 
-    public void start(Activity startActivity) {
+    /**
+     * Start screen flow
+     * @param startActivity
+     */
+    public void start(Activity startActivity, Integer condition) {
         mScreenFlowManager.setCurrentScreenFlow(this);
-        this.mStartedActivity = startActivity.getClass();
         // configure screen flow at runtime
         // TODO change it to static?
-        configureScreenFlow(mStartedActivity);
+        configureScreenFlow(startActivity.getClass());
         // start next screen
-        next(startActivity);
+        next(startActivity, condition);
+    }
+
+    /**
+     * Start screen flow
+     * @param startActivity
+     * @see #start(Activity, Integer)
+     */
+    public void start(Activity startActivity) {
+        start(startActivity, ScreenNode.NORMAL_COMPLETE);
     }
 
     /**
@@ -30,8 +46,40 @@ public abstract class ScreenFlow {
      * @param activity
      *  previous screen.
      */
-    public void next(Activity activity) {
+    public void next(Activity activity, Integer condition) {
+        // get current screen node
+        ScreenNode screenNode = mScreenNodeMap.get(activity.getClass());
+        ScreenNode nextScreenNode = screenNode.getNext(condition);
+        Class<? extends Activity> nextActivityClass = nextScreenNode.getCurrent();
+        // start next screen
+        Intent intent = new Intent(activity, nextActivityClass);
+        activity.startActivity(intent);
+    }
 
+    /**
+     * Start next screen which known from configuration.
+     * @param activity
+     * @see #next(Activity, Integer)
+     */
+    public void next(Activity activity) {
+        next(activity, ScreenNode.NORMAL_COMPLETE);
+    }
+
+    /**
+     * Current screen node which hold next step information
+     * @param activityClass
+     * @return
+     */
+    public ScreenNode from(Class<? extends Activity> activityClass) {
+        ScreenNode screenNode;
+        if (mScreenNodeMap.containsKey(activityClass)) {
+            screenNode = mScreenNodeMap.get(activityClass);
+        } else {
+            screenNode = new ScreenNode();
+            screenNode.to(activityClass);
+            mScreenNodeMap.put(activityClass, screenNode);
+        }
+        return screenNode;
     }
 
     protected abstract void configureScreenFlow(Class<? extends Activity> startedActivity);
