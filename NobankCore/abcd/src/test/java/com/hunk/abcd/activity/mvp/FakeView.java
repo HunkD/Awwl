0f6b2e0ee1b14db8c84bd1dc18abc4bde7f7c7cc
Hunk Deng <niknow@live.com>
2016-07-03 10:06:38 +0800

    [abcd] Add unit test for mvp package.

---
+++ NobankCore/abcd/src/test/java/com/hunk/abcd/activity/mvp/FakeView.java
@@ -0,0 +1,24 @@
+package com.hunk.abcd.activity.mvp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.hunk.abcd.R;
+
+/**
+ * @author HunkDeng
+ * @since 2016/7/3
+ */
+public class FakeView
+        extends AbstractViewActivity<IFakePresenter>
+        implements IFakeView<IFakePresenter> {
+    {
+        setPresenter(new FakePresenter());
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ NobankCore/abcd/src/test/java/com/hunk/abcd/activity/mvp/IFakePresenter.java
@@ -0,0 +1,8 @@
+package com.hunk.abcd.activity.mvp;
+
+/**
+ * @author HunkDeng
+ * @since 2016/7/3
+ */
+public interface IFakePresenter<V extends BaseView> extends BasePresenter<V> {
+}
