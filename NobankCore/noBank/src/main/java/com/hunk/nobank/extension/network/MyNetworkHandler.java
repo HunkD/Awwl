d3bde4a4e295739ead9c06fb71a4087a4571c895
Hunk Deng <niknow@live.com>
2016-07-10 13:54:05 +0800

    [nobank] Switch to use Rxjava's scheduler IO thread to handle network operation, previously we use multiple thread created by Okhttp. so we won't create wild thread in here by Okhttp.

--- NobankCore/noBank/src/main/java/com/hunk/nobank/extension/network/MyNetworkHandler.java
+++ NobankCore/noBank/src/main/java/com/hunk/nobank/extension/network/MyNetworkHandler.java
@@ -3,7 +3,6 @@ package com.hunk.nobank.extension.network;
 import com.hunk.nobank.contract.RealResp;
 import com.hunk.nobank.model.Cacheable;
 import com.hunk.abcd.extension.log.Logging;
-import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
@@ -48,46 +47,36 @@ public class MyNetworkHandler extends NetworkHandler {
         return Observable.create(new Observable.OnSubscribe<R>() {
             @Override
             public void call(final Subscriber<? super R> subscriber) {
-                okHttpClient.newCall(request).enqueue(new Callback() {
-                    @Override
-                    public void onFailure(Request request, IOException e) {
-                        // TODO: NanoHTTPD have problem on quickly sequence call, so add interval time here.
-                        try {
-                            Thread.sleep(5000);
-                        } catch (InterruptedException e1) {
-                            e1.printStackTrace();
-                        }
-                        subscriber.onError(e);
-                        subscriber.onCompleted();
+                try {
+                    Response response = okHttpClient.newCall(request).execute();
+
+                    // TODO: NanoHTTPD have problem on quickly sequence call, so add interval time here.
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e1) {
+                        e1.printStackTrace();
                     }
 
-                    @Override
-                    public void onResponse(Response response) throws IOException {
-                        // TODO: NanoHTTPD have problem on quickly sequence call, so add interval time here.
-                        try {
-                            Thread.sleep(5000);
-                        } catch (InterruptedException e1) {
-                            e1.printStackTrace();
-                        }
-                        String json = response.body().string();
-                        Logging.d("network response = " + json);
-                        if (response.isSuccessful()) {
-                            RealResp<R> realResp = getRealResponse(json, req.getResponseType());
-                            if (isRespSuccessfully(realResp)) {
-                                if (req instanceof Cacheable) {
-                                    ((Cacheable<R>)req).setCache(realResp.Response, req);
-                                }
-                                subscriber.onNext(realResp.Response);
-                            } else {
-                                subscriber.onError(new ServerError(0));
+                    String json = response.body().string();
+                    Logging.d("network response = " + json);
+                    if (response.isSuccessful()) {
+                        RealResp<R> realResp = getRealResponse(json, req.getResponseType());
+                        if (isRespSuccessfully(realResp)) {
+                            if (req instanceof Cacheable) {
+                                ((Cacheable<R>)req).setCache(realResp.Response, req);
                             }
+                            subscriber.onNext(realResp.Response);
                         } else {
-                            subscriber.onError(new ServerError(NetworkHandler.NETWORK_ERROR));
-
+                            subscriber.onError(new ServerError(0));
                         }
-                        subscriber.onCompleted();
+                    } else {
+                        subscriber.onError(new ServerError(NetworkHandler.NETWORK_ERROR));
+
                     }
-                });
+                    subscriber.onCompleted();
+                } catch (IOException e) {
+                    subscriber.onError(new ServerError(NetworkHandler.NETWORK_ERROR));
+                }
             }
         }).subscribeOn(Schedulers.io());
     }
